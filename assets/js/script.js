9c7142ceaceca9d0f669cce92a27fc82372526dc
Royden Semedo <roydensemedo@Roydens-MacBook-Pro.local>
2022-03-26 16:11:58 -0400

    finished code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,147 +1,78 @@
-class TimeblockObj {
-    constructor(hour, todo) {
-      this.hour = hour;
-      this.todo = todo;
-    }
-  }
-  
-  window.onload = function() {
-    const currentTimeblocks = getCurrentTimeblocks();
-    const currentTime = moment();
-  
-    displayCurrentDate(currentTime);
-    displayTimeblockRows(currentTime);
-  
-    document.querySelector('.container')
-      .addEventListener('click', function(event) {
-        containerClicked(event, currentTimeblocks);
-      });
-    setTimeblockText(currentTimeblocks);
-  };
-  
-  function getCurrentTimeblocks() {
-    const currentTimeblocks = localStorage.getItem('timeblockObjects');
-    return currentTimeblocks ? JSON.parse(currentTimeblocks) : [];
-  }
-  
-  function displayCurrentDate(currentTime) {
-    document.getElementById('currentDay')
-      .textContent = currentTime.format('dddd, MMMM Do');
-  }
-  
-  /*** functions for displaying all timeblock rows ***/
-  function displayTimeblockRows(currentTime) {
-    const currentHour = currentTime.hour();
-    //working hours are 9-5 or 9-17
-    for (let i = 9; i <= 17; i ++) {
-      const timeblock = createTimeblockRow(i);
-      const hourCol = createCol(createHourDiv(i), 1);
-      const textArea = createCol(createTextArea(i, currentHour), 10);
-      const saveBtn = createCol(createSaveBtn(i), 1);
-      appendTimeblockColumns(timeblock, hourCol, textArea, saveBtn);
-      document.querySelector('.container').appendChild(timeblock);
-    }
-  }
-  
-  function createTimeblockRow(hourId) {
-    const timeblock = document.createElement('div');
-    timeblock.classList.add('row');
-    timeblock.id = `timeblock-${hourId}`;
-    return timeblock;
-  }
-  
-  function createCol(element, colSize) {
-    const col = document.createElement('div');
-    col.classList.add(`col-${colSize}`,'p-0');
-    col.appendChild(element);
-    return col;
-  }
-  
-  function createHourDiv(hour) {
-    const hourCol = document.createElement('div');
-    hourCol.classList.add('hour');
-    hourCol.textContent = formatHour(hour);
-    return hourCol;
-  }
-  
-  function formatHour(hour) {
-    const hourString = String(hour);
-    return moment(hourString, 'h').format('hA');
-  }
-  
-  function createTextArea(hour, currentHour) {
-    const textArea = document.createElement('textarea');
-    textArea.classList.add(getTextAreaBackgroundClass(hour, currentHour));
-    return textArea;
-  }
-  
-  function getTextAreaBackgroundClass(hour, currentHour) {
-    return hour < currentHour ? 'past' 
-      : hour === currentHour ? 'present' 
-      : 'future';
-  }
-  
-  function createSaveBtn(hour) {
-    const saveBtn = document.createElement('button');
-    saveBtn.classList.add('saveBtn');
-    saveBtn.innerHTML = '<i class="fas fa-save"></i>';
-    saveBtn.setAttribute('data-hour', hour);
-    return saveBtn;
-  }
-  
-  function appendTimeblockColumns(timeblockRow, hourCol, textAreaCol, saveBtnCol) {
-    const innerCols = [hourCol, textAreaCol, saveBtnCol];
-    for (let col of innerCols) {
-      timeblockRow.appendChild(col);
-    }
-  }
-  
-  /*** functions for saving to local storage ***/
-  function containerClicked(event, timeblockList) {
-    if (isSaveButton(event)) {
-      const timeblockHour = getTimeblockHour(event);
-      const textAreaValue = getTextAreaValue(timeblockHour);
-      placeTimeblockInList(new TimeblockObj(timeblockHour, textAreaValue), timeblockList);
-      saveTimeblockList(timeblockList);
-    }
-  }
-  
-  function isSaveButton(event) {
-    return event.target.matches('button') || event.target.matches('.fa-save');
-  }
-  
-  function getTimeblockHour(event) {
-    return event.target.matches('.fa-save') ? event.target.parentElement.dataset.hour : event.target.dataset.hour;
-  }
-  
-  function getTextAreaValue(timeblockHour) {
-    return document.querySelector(`#timeblock-${timeblockHour} textarea`).value;
-  }
-  
-  function placeTimeblockInList(newTimeblockObj, timeblockList) {
-    if (timeblockList.length > 0) {
-      for (let savedTimeblock of timeblockList) {
-        if (savedTimeblock.hour === newTimeblockObj.hour) {
-          savedTimeblock.todo = newTimeblockObj.todo;
-          return;
+//declaring variables to target the Ids from index.html
+var current = $("#currentDay");
+//using moment.js to display the currentDay in m/d/y and time format
+const currentDay = moment().format("MMMM Do YYYY, h:mm:ss a ");
+//converting hours into integer from string using parseInt
+const currentHour = parseInt(moment().hour());
+//display currentDay on the page
+current.text(currentDay);
+const blockLength = $(".time-block").length;
+
+
+
+    renderLocalStorage();
+
+        $(".description").each(function() {
+            //_i = input used to create the object, converting integer tostring 
+            // targeting description block to add colors
+            let currentBlockTimeVal = parseInt(moment($(this).attr("id"))._i);
+            if (moment(currentBlockTimeVal).isBefore(currentHour)) {
+                $(this).addClass("past");
+            }
+            else if (moment(currentBlockTimeVal).isSame(currentHour)) {
+                $(this).addClass("present");
+            }
+            else if (moment(currentBlockTimeVal).isAfter(currentHour)) {
+                $(this).addClass("future");
+            }
+        });
+
+        $(".saveBtn").on("click", function() {
+            for(let i = 0; i < blockLength; i++) {
+                let currentID = $(".description").eq(i).attr("id");
+                let value = $(".description").eq(i).val().trim();
+                localStorage.setItem(currentID, value);
+            }
+        });
+
+
+        function renderLocalStorage() {
+            for (let i = 9, j = 0; i < 18; i++, j++) {
+                let blockInfo = (localStorage.getItem(i))
+                $(".description").eq(j).text(blockInfo)
+            }
         }
-      }
-    } 
-    timeblockList.push(newTimeblockObj);
-    return;
+
+const rows = document.getElementsByClassName("row");
+
+Array.from(rows).forEach(row => {
+  let
+    rowIdString = row.id,
+    rowHour;
+  if (rowIdString) {
+    rowHour = parseInt(rowIdString);
   }
-  
-  function saveTimeblockList(timeblockList) {
-    localStorage.setItem('timeblockObjects', JSON.stringify(timeblockList));
-  }
-  
-  function setTimeblockText(timeblockList) {
-    if (timeblockList.length === 0 ) {
-      return;
+  if (rowHour) {
+    // Compares row id to current hour and sets color accordingly
+    if (currentHour === rowHour) {
+      setColor(row, "red");
+    } else if ((currentHour < rowHour) && (currentHour > rowHour - 6)) {
+      setColor(row, "green");
+    } else if ((currentHour > rowHour) && (currentHour < rowHour + 6)) {
+      setColor(row, "lightgrey");
     } else {
-      for (let timeblock of timeblockList) {
-        document.querySelector(`#timeblock-${timeblock.hour} textarea`)
-          .value = timeblock.todo;
-      }
+      setColor(row, "white");
     }
   }
+});
+
+function setColor(element, color) {
+  element.style.backgroundColor = color;
+}
+var timeChange = function(){
+  //jumbotron time
+  const timeNow = moment().format("dddd, MMM Do, YYYY, hh:mm:ss a");
+  current.text(timeNow);
+}
+
+setInterval(timeChange, 1000);
